bb77e23d67e0acb96c858bc49bf0c1615bb18de8
kensyo <takitateokowa@gmail.com>
2023-02-16 23:55:02 +0900

    feat: remove warnings

--- app/entry.js
+++ app/entry.js
@@ -1,9 +1,12 @@
 'use strict'
 
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App.js";
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
 
 // import FDRS from '3NF_SYNTHESIS'
 
