b7a1cda63b12e227b9acedac50b4f7b38feccd4b
kensyo <takitateokowa@gmail.com>
2023-02-19 23:50:42 +0900

    refactor: lift up states from Info to App

--- app/js/App.js
+++ app/js/App.js
@@ -11,6 +11,73 @@ const attributes_state_name = "attributes_input"
 const FDs_state_name = "FDs"
 
 class Info extends Component {
+  get_attributes(str = this.props[attributes_state_name]) {
+    return Array.from(new Set(str.split(',').map(x => x.trim()).filter(x => x)))
+  }
+
+  render() {
+    const attributes = this.get_attributes()
+    const options = attributes.map(attr => {
+      return { value: attr, label: attr }
+    })
+
+    const should_put_placeholder =
+      this.props[name_state_name] === "" &&
+      this.props[attributes_state_name] === ""
+
+    return (
+      <div>
+        <p>Enter your scheme information.</p>
+        <div>
+          <label>Name:</label>
+          <input
+            value={this.props[name_state_name]}
+            onChange={event => this.props.handleInputChange(name_state_name, event)}
+            type="text"
+            className="relation_name"
+            size="40"
+            maxLength="50"
+            placeholder={should_put_placeholder ? "example: vegetables" : ""} />
+        </div>
+        <div>
+          <label>Attributes:</label>
+          <input
+            value={this.props[attributes_state_name]}
+            onChange={event => this.props.handleInputChange(attributes_state_name, event)}
+            type="text"
+            className="relation_attributes"
+            size="40"
+            maxLength="500"
+            placeholder={should_put_placeholder ? "name,grower,growing_area,price" : ""}
+          />
+        </div>
+        <div>
+          <label>Functional Dependencies:</label>
+          {
+            this.props[FDs_state_name].map((fd, num) =>
+              <FD
+                key={num}
+                options={options}
+                should_put_placeholder={should_put_placeholder}
+                leftValue={fd[0]}
+                rightValue={fd[1]}
+                onLeftSelectChange={event => this.props.handleLeftSelectChange(num, event)}
+                onRightSelectChange={event => this.props.handleRightSelectChange(num, event)}
+                onButtonClick={event => this.props.handleButtonClick(num, event)}
+              />
+            )
+          }
+          <button onClick={this.props.handleAddButtonClick}>
+            Add another FD
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+// <FD attributes={attributes} options={options} should_put_placeholder={should_put_placeholder} />
+
+class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -20,8 +87,11 @@ class Info extends Component {
         [[], []]
       ]
     }
-    this.handleChange = this.handleInputChange.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
     this.handleButtonClick = this.handleButtonClick.bind(this)
+    this.handleAddButtonClick = this.handleAddButtonClick.bind(this)
+    this.handleLeftSelectChange = this.handleLeftSelectChange.bind(this)
+    this.handleRightSelectChange = this.handleRightSelectChange.bind(this)
   }
 
   get_attributes(str = this.state[attributes_state_name]) {
@@ -89,92 +159,28 @@ class Info extends Component {
     })
   }
 
-  render() {
-    const attributes = this.get_attributes()
-    const options = attributes.map(attr => {
-      return { value: attr, label: attr }
+  handleAddButtonClick(event) {
+    const new_FDs = this.state[FDs_state_name].slice()
+    new_FDs.push([[], []])
+    this.setState({
+      [FDs_state_name]: new_FDs
     })
-
-    const should_put_placeholder =
-      this.state[name_state_name] === "" &&
-      this.state[attributes_state_name] === ""
-
-    return (
-      <div>
-        <p>Enter your scheme information.</p>
-        <div>
-          <label>Name:</label>
-          <input
-            value={this.state[name_state_name]}
-            onChange={event => this.handleInputChange(name_state_name, event)}
-            type="text"
-            className="relation_name"
-            size="40"
-            maxLength="50"
-            placeholder={should_put_placeholder ? "example: vegetables" : ""} />
-        </div>
-        <div>
-          <label>Attributes:</label>
-          <input
-            value={this.state[attributes_state_name]}
-            onChange={event => this.handleInputChange(attributes_state_name, event)}
-            type="text"
-            className="relation_attributes"
-            size="40"
-            maxLength="500"
-            placeholder={should_put_placeholder ? "name,grower,growing_area,price" : ""}
-          />
-        </div>
-        <div>
-          <label>Functional Dependencies:</label>
-          {
-            this.state[FDs_state_name].map((fd, num) =>
-              <FD
-                key={num}
-                options={options}
-                should_put_placeholder={should_put_placeholder}
-                leftValue={fd[0]}
-                rightValue={fd[1]}
-                onLeftSelectChange={event => this.handleLeftSelectChange(num, event)}
-                onRightSelectChange={event => this.handleRightSelectChange(num, event)}
-                onButtonClick={event => this.handleButtonClick(num, event)}
-              />
-            )
-          }
-          <button onClick={(event) => {
-            const new_FDs = this.state[FDs_state_name].slice()
-            new_FDs.push([[], []])
-            this.setState({
-              [FDs_state_name]: new_FDs
-            })
-          }
-          }>
-            Add another FD
-          </button>
-        </div>
-      </div>
-    )
-  }
-}
-// <FD attributes={attributes} options={options} should_put_placeholder={should_put_placeholder} />
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      scheme: new FdRelationScheme(
-        "",
-        [],
-        []
-      )
-    }
   }
 
   render() {
     return (
       <div className="App">
         <h1>Normalize and investigate fd scheme</h1>
-        <Info />
+        <Info
+          name_input={this.state.name_input}
+          attributes_input={this.state.attributes_input}
+          FDs={this.state.FDs}
+          handleInputChange={this.handleInputChange}
+          handleLeftSelectChange={this.handleLeftSelectChange}
+          handleRightSelectChange={this.handleRightSelectChange}
+          handleButtonClick={this.handleButtonClick}
+          handleAddButtonClick={this.handleAddButtonClick}
+        />
       </div>
     );
   }
