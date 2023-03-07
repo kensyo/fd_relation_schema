3c1e1c723902a2e64585feb4bddf9b51c767ca97
kensyo <takitateokowa@gmail.com>
2023-03-07 12:28:35 +0900

    refactor: remove unnecessary imports

--- app/js/App.js
+++ app/js/App.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import { Grid, Stack, Container, Typography, Box, InputLabel, FormHelperText, Button } from '@mui/material';
+import { Grid, Stack, Container, Typography, Box, Button } from '@mui/material';
 import Name from './Name';
 import Attributes from './Attributes';
 import FD from './FD';
