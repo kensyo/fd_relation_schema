44e5fcf177d88161d068eef9da1229cb7406a712
kensyo <takitateokowa@gmail.com>
2023-03-07 10:25:00 +0900

    feat: number fds

--- app/js/FD.js
+++ app/js/FD.js
@@ -7,6 +7,8 @@ export default (props) => {
   const dispatch = props.dispatch
   const options = props.options
 
+  const indexPlus1 = index + 1
+
   const setFDs = (newFDs) => {
     dispatch({ type: "fds_change", value: newFDs })
   }
@@ -29,66 +31,50 @@ export default (props) => {
     setFDs(newFDs)
   }
 
+  const renderAutocomplete = (whichValue, whichSide, whichHandleChange) => {
+    return (
+      <Autocomplete
+        multiple
+        filterSelectedOptions
+        value={whichValue}
+        onChange={whichHandleChange}
+        options={options}
+        renderTags={(value, getTagProps) =>
+          value.map((option, index) => (
+            <Chip
+              color="secondary"
+              variant="outlined"
+              label={option.label}
+              size="small"
+              {...getTagProps({ index })} />
+          ))
+        }
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            label={`${whichSide} of FD${indexPlus1}`}
+            size="small"
+            InputLabelProps={{ shrink: true }}
+          />
+        )}
+      />
+    )
+  }
+
   return (
     <React.Fragment>
+      <Grid item xs={0}>
+        {`${indexPlus1}. `}
+      </Grid>
       <Grid item xs={5}>
-        <Autocomplete
-          multiple
-          filterSelectedOptions
-          value={props.leftValue}
-          onChange={handleLeftChange}
-          options={options}
-          renderTags={(value, getTagProps) =>
-            value.map((option, index) => (
-              <Chip
-                color="secondary"
-                variant="outlined"
-                label={option.label}
-                size="small"
-                {...getTagProps({ index })} />
-            ))
-          }
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              variant="outlined"
-              label="LHS of FD"
-              size="small"
-              InputLabelProps={{ shrink: true }}
-            />
-          )}
-        />
+        {renderAutocomplete(props.leftValue, "LHS", handleLeftChange)}
       </Grid>
       <Grid item xs={0}>
         ➞
       </Grid>
       <Grid item xs={5}>
-        <Autocomplete
-          multiple
-          filterSelectedOptions
-          value={props.rightValue}
-          onChange={handleRightChange}
-          options={options}
-          renderTags={(value, getTagProps) =>
-            value.map((option, index) => (
-              <Chip
-                color="secondary"
-                variant="outlined"
-                label={option.label}
-                size="small"
-                {...getTagProps({ index })} />
-            ))
-          }
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              variant="outlined"
-              label="RHS of FD"
-              size="small"
-              InputLabelProps={{ shrink: true }}
-            />
-          )}
-        />
+        {renderAutocomplete(props.rightValue, "RHS", handleRightChange)}
       </Grid>
       <Grid item xs={0}>
         <Button variant="outlined" color="error" onClick={handleClick}>
@@ -98,64 +84,3 @@ export default (props) => {
     </React.Fragment>
   )
 }
-
-
-// +import React from "react";
-//  import Select from "react-select";
-// +import { Grid, Button } from '@mui/material'
-// +export default (props) => {
-// +  const fds = props.fds
-// +  const index = props.index
-// +  const dispatch = props.dispatch
-// +  const setFDs = (newFDs) => {
-// +    dispatch({ type: "fds_change", value: newFDs })
-// +  }
-// +  const handleLeftChange = (event) => {
-// +    const newFDs = [...fds]
-// +    newFDs[index][0] = event
-// +    setFDs(newFDs)
-// +  }
-// +
-// +  const handleRightChange = (event) => {
-// +    const newFDs = [...fds]
-// +    newFDs[index][1] = event
-// +    setFDs(newFDs)
-//    }
-// +  const handleClick = () => {
-// +    const newFDs = [...fds]
-// +    newFDs.splice(index, 1)
-// +    setFDs(newFDs)
-// +  }
-// +
-// +  return (
-// +    <React.Fragment>
-// +      <Grid item xs={5}>
-// +        <Select
-// +          isMulti
-// +          className="lhs"
-// +          value={props.leftValue}
-// +          onChange={handleLeftChange}
-// +          options={props.options}
-// +        />
-// +      </Grid>
-// +      <Grid item xs={0}>
-// +        ➞
-// +      </Grid>
-// +      <Grid item xs={5}>
-// +        <Select
-// +          isMulti
-// +          className="rhs"
-// +          value={props.rightValue}
-// +          onChange={handleRightChange}
-// +          options={props.options}
-// +        />
-// +      </Grid>
-// +      <Grid item xs={0}>
-// +        <Button variant="outlined" color="error" onClick={handleClick}>
-// +          Remove
-// +        </Button>
-// +      </Grid>
-// +    </React.Fragment>
-// +  )
-// +}
-
